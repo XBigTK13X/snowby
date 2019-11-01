5c16b110aeb32a05927b76e7560fe49a60242592
Tim Kretschmer <xbigtk13x@gmail.com>
2019-11-01 10:16:20 -0500

    Improve audio keep awake to support 7.1 stereo

--- src/service/audio.js
+++ src/service/audio.js
@@ -1,9 +1,9 @@
-global.TONE_SILENCE_LOGGING = true
 const settings = require('../settings')
 const spawn = require('child_process').spawn
-let wakeInterval = null
+const util = require('../util')
 
 let audioProcess
+let resetProcess
 
 const cleanup = () => {
     if (audioProcess) {
@@ -16,8 +16,13 @@ const keepAwake = () => {
     cleanup()
     if (settings.keepAudioDeviceAwake) {
         console.log(`Waking audio device by looping ${settings.inaudibleWavPath}`)
-        audioProcess = spawn(`powershell`, [`-c`, `while($true){(New-Object Media.SoundPlayer "${settings.inaudibleWavPath}").PlaySync();}`])
+        audioProcess = spawn(settings.mpvExePath, [`${settings.inaudibleWavPath}`, `--loop-file=inf`,`--vo=null`, `--no-config`])// DEBUG ->, `--log-file=${util.appPath('/bin/mpv/mpv-audio-keep-awake.log')}`])
     }
+}
+
+// Untested
+const resetDeviceSync = () => {
+    resetProcess = spawn('powershell',[util.appPath('/bin/audio/reset-connection.ps1')])
 }
 
 process.on('exit', function() {
