67c658447b9528224988dee5c6bcd86b6de42d6d
Tim Kretschmer <xbigtk13x@gmail.com>
2020-06-02 06:18:18 -0500

    Improve mpv reliability and prevent hanging processes
    Upgrade dependencies. Keeping tippy at v5 since v6 requires migration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-const { app, BrowserWindow, ipcMain, shell } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const settings = require('./settings')
 const util = require('./util')
-const spawn = require('child_process').spawn
-const audio = require('./service/audio')
-const MpvSocket = require('./service/mpv-socket')
+const audio = require('./desktop/audio')
+const ipcServer = require('./desktop/ipc-server')
 
 let mainWindow = null
 
@@ -62,86 +61,4 @@ app.on('window-all-closed', () => {
     }
 })
 
-ipcMain.on('snowby-exit', () => {
-    app.quit(0)
-})
-
-ipcMain.on('snowby-get-media-profiles', event => {
-    event.returnValue = util.getMediaProfiles()
-})
-
-ipcMain.on('snowby-wake-audio', () => {
-    audio.keepAwake()
-})
-
-ipcMain.on('snowby-open-website', (event, url) => {
-    spawn('cmd.exe', [`/c start firefox ${url}`], settings.spawnOptions)
-    //spawn('cmd.exe', [`/c start chrome ${url}`], settings.spawnOptions)
-    //spawn('cmd.exe', [`/c start microsoft-edge:${url}`], settings.spawnOptions)
-})
-
-let mpvSocket = null
-let mpvProcess = null
-
-ipcMain.on('snowby-launch-mpv', (event, options) => {
-    try {
-        let defaults = ['--msg-level=ipc=v', `--input-ipc-server=${settings.mpvSocketPath}`]
-        let args = defaults.concat(options)
-        if (mpvProcess) {
-            if (settings.debugMpvSocket) {
-                console.log('Killing')
-            }
-            mpvProcess.kill()
-            mpvSocket.quit()
-        }
-        if (settings.debugMpvSocket) {
-            console.log('args', args)
-        }
-        mpvProcess = spawn(settings.mpvExePath, args)
-        if (settings.debugMpvSocket) {
-            console.log('Process spawned')
-        }
-        let connectInterval = setInterval(() => {
-            if (!mpvSocket || (mpvSocket && !mpvSocket.getIsConnected())) {
-                mpvSocket = new MpvSocket(settings.mpvSocketPath)
-                mpvSocket.connect()
-            }
-            if (mpvSocket && mpvSocket.getIsConnected()) {
-                clearInterval(connectInterval)
-                if (settings.debugMpvSocket) {
-                    console.log('Socket connected')
-                }
-
-                event.returnValue = 'success'
-            }
-        }, 100)
-    } catch {
-        event.returnValue = 'failure'
-    }
-})
-
-ipcMain.on('snowby-is-mpv-running', event => {
-    try {
-        let result = mpvSocket.getIsConnected()
-        event.returnValue = result
-    } catch {
-        event.returnValue = false
-    }
-})
-
-ipcMain.on('snowby-get-mpv-position', async event => {
-    try {
-        event.returnValue = await mpvSocket.getProperty('time-pos')
-    } catch {
-        event.returnValue = null
-    }
-})
-
-process.on('exit', function() {
-    if (settings.debugMpvSocket) {
-        console.log('Exiting')
-    }
-    if (mpvProcess) {
-        mpvProcess.kill()
-    }
-})
+ipcServer.server.listen()
