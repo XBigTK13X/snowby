9cf240ede8af1d85e70f6962735b7a70f2bd516b
Tim Kretschmer <xbigtk13x@gmail.com>
2020-09-12 09:11:11 -0500

    OTA channel mapping update

--- src/settings.js
+++ src/settings.js
@@ -22,7 +22,7 @@ let config = {
     mpvSocketPath: '\\\\.\\pipe\\snowby-mpv-ipc',
     progressUpdateInterval: 3000,
     stepBackSeconds: 4,
-    versionDate: 'September 11, 2020',
+    versionDate: 'September 12, 2020',
     windowBackgroundColor: '#010101',
     progressWatchedThreshold: {
         minPercent: 5,
