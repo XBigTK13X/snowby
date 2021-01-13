461774bdeec5a834b5ba13cafd111d18e8550bea
Tim Kretschmer <xbigtk13x@gmail.com>
2021-01-12 21:52:57 -0600

    Improve HDR toggle, progress tracking, and launch times on low spec systems

--- src/media/progress.js
+++ src/media/progress.js
@@ -16,39 +16,48 @@ let trackInterval = null
 
 const track = (embyItem, audioRelativeIndex, subtitleRelativeIndex, resumeButton, resumeContent, isHdr) => {
     let lastTicks = -1
+    let mutex = false
     if (trackInterval) {
         clearInterval(trackInterval)
     }
     trackInterval = setInterval(() => {
-        player
-            .connect()
-            .then(() => {
-                player
-                    .getPositionInEmbyTicks()
-                    .then((playbackPositionTicks) => {
-                        if (playbackPositionTicks != lastTicks && playbackPositionTicks != null) {
-                            lastTicks = playbackPositionTicks
-                            setConnectionStatus(true)
-                            if (playbackPositionTicks > 0) {
-                                emby.client.updateProgress(embyItem.Id, playbackPositionTicks, embyItem.RunTimeTicks).then(() => {
-                                    let newParams = util.queryParams()
-                                    newParams.resumeTicks = playbackPositionTicks
-                                    window.reloadPage(`play-media.html?${util.queryString(newParams)}`)
-                                })
+        if (!mutex) {
+            mutex = true
+            player
+                .connect()
+                .then(() => {
+                    player
+                        .getPositionInEmbyTicks()
+                        .then((playbackPositionTicks) => {
+                            if (playbackPositionTicks != lastTicks && playbackPositionTicks != null) {
+                                lastTicks = playbackPositionTicks
+                                setConnectionStatus(true)
+                                if (playbackPositionTicks > 0) {
+                                    emby.client.updateProgress(embyItem.Id, playbackPositionTicks, embyItem.RunTimeTicks).then(() => {
+                                        let newParams = util.queryParams()
+                                        newParams.resumeTicks = playbackPositionTicks
+                                        window.reloadPage(`play-media.html?${util.queryString(newParams)}`)
+                                        mutex = false
+                                    })
+                                }
                             }
-                        }
-                    })
-                    .catch((mediaFinished) => {
+                        })
+                        .catch((mediaFinished) => {
+                            util.clientLog('Media finished playing ' + embyItem.Path)
+                            setConnectionStatus(false)
+                            clearInterval(trackInterval)
+                            mutex = false
+                        })
+                })
+                .catch((err) => {
+                    if (err === 'disconnected') {
+                        util.clientLog('Media disconnected ' + embyItem.Path)
                         setConnectionStatus(false)
                         clearInterval(trackInterval)
-                    })
-            })
-            .catch((err) => {
-                if (err === 'disconnected') {
-                    setConnectionStatus(false)
-                    clearInterval(trackInterval)
-                }
-            })
+                        mutex = false
+                    }
+                })
+        }
     }, settings.progressUpdateInterval)
 }
 
